1dd54366c5bdccecae2de8ae3c02ff0f2f492e72
ErhanTK <125463792+ErhanTK@users.noreply.github.com>
2023-12-07 14:15:50 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        # Convert item to string
+        key = str(item)
+        # Increment the count for this key in the dictionary
+        if key in frequencies:
+            frequencies[key] += 1
+        else:
+            frequencies[key] = 1
     return frequencies
+
